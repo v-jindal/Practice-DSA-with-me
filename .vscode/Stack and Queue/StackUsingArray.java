1f078e4ad1a57e1d9bb822da329e09086728d5a9
v-jindal <vanshikajindal548@gmail.com>
2026-07-31 00:41:45 +0530

    Auto Backup - 2026-07-31 00:41

--- .vscode/Stack and Queue/StackUsingArray.java
+++ .vscode/Stack and Queue/StackUsingArray.java
@@ -1,3 +1,48 @@
 public class StackUsingArray {
-    
+    int i = -1;
+    int stack[];
+
+    public StackUsingArray(int n) {
+        stack = new int[n];
+    }
+
+    public boolean isEmpty() {
+        if (i == -1)
+            return true;
+        return false;
+    }
+
+    public boolean isFull() {
+        if (i == stack.length - 1)
+            return true;
+        return false;
+    }
+
+    public void push(int x) {
+        if (i >= stack.length - 1)
+            return;
+        i++;
+        stack[i] = x;
+    }
+
+    public void pop() {
+        if (i != -1)
+            i--;
+    }
+
+    public int peek() {
+        if (isEmpty())
+            return -1;
+        return stack[i];
+    }
+
+    public static void main(String[] args) {
+        StackUsingArray stack = new StackUsingArray(5);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        System.out.println(stack.peek());
+        stack.pop();
+        System.out.println(stack.peek());
+    }
 }
