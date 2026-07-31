7786dca5f951b552148c7b9e3a0551edcb995742
v-jindal <vanshikajindal548@gmail.com>
2026-08-01 00:17:53 +0530

    Auto Backup - 2026-08-01 00:17

--- .vscode/Stack and Queue/StackUsingLinkedList.java
+++ .vscode/Stack and Queue/StackUsingLinkedList.java
@@ -1,3 +1,49 @@
+class Node {
+    int data;
+    Node next;
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
 public class StackUsingLinkedList {
-    
+    Node n;
+    int i = 0;
+    public StackUsingLinkedList() {
+    }
+    public boolean isEmpty() {
+        return n == null;
+    }
+
+    public void push(int x) {
+        Node top = new Node(x);
+        top.next = n;
+        n = top;
+        i++;
+    }
+
+    public void pop() {
+        if(n != null){
+            n = n.next;
+            i--;
+        }
+    }
+
+    public int peek() {
+        if(isEmpty()) return -1;
+        return n.data;
+    }
+
+    public int size() {
+        return i;
+    }
+    public static void main(String[] args) {
+        StackUsingLinkedList stack = new StackUsingLinkedList();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        System.out.println(stack.peek());
+        stack.pop();
+        System.out.println(stack.peek());
+    }
 }
