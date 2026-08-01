dcd1ddb51b71cec6608b06576a4e6d0eab21af4c
v-jindal <vanshikajindal548@gmail.com>
2026-08-01 08:44:40 +0530

    Auto Backup - 2026-08-01 08:44

--- .vscode/Stack and Queue/Leetcode844.java
+++ .vscode/Stack and Queue/Leetcode844.java
@@ -1,3 +1,29 @@
 public class Leetcode844 {
-    
+    public static boolean backspaceCompare(String s, String t) {
+        StringBuilder sb = new StringBuilder();
+        for(char c : s.toCharArray()){
+            if(c != '#') sb.append(c);
+            else{
+                if(sb.length() > 0){
+                    sb.deleteCharAt(sb.length() - 1);
+                }
+            }
+        }
+        StringBuilder tb = new StringBuilder();
+        for(char c : t.toCharArray()){
+            if(c != '#') tb.append(c);
+            else{
+                if(tb.length() > 0){
+                    tb.deleteCharAt(tb.length() - 1);
+                }
+            }
+        }
+        return sb.toString().equals(tb.toString());
+    }
+    public static void main(String[] args) {
+        System.out.println(backspaceCompare("ab#c", "ad#c")); 
+        System.out.println(backspaceCompare("ab##", "c#d#")); 
+        System.out.println(backspaceCompare("a##c", "#a#c")); 
+        System.out.println(backspaceCompare("a#c", "b")); 
+    }
 }
