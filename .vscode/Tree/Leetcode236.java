e7b23a78462349413d34bd66a78c8b34a01e7469
v-jindal <vanshikajindal548@gmail.com>
2026-08-02 21:24:58 +0530

    Auto Backup - 2026-08-02 21:24

--- .vscode/Tree/Leetcode236.java
+++ .vscode/Tree/Leetcode236.java
@@ -1,3 +1,25 @@
 public class Leetcode236 {
-    
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root == p || root == q){
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left == null) return right;
+        if(right == null) return left;
+        return root;
+    }
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(1);    
+        root.left.left = new TreeNode(6);
+        root.left.right = new TreeNode(2);
+        root.right.left = new TreeNode(0);
+        root.right.right = new TreeNode(8);
+        TreeNode p = root.left.left;
+        TreeNode q = root.right.left;
+        TreeNode lca = lowestCommonAncestor(root, p, q);
+        System.out.println("Lowest Common Ancestor of " + p.val + " and " + q.val + " is: " + lca.val);
+    }
 }
