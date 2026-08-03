a252fd4bc54de2e8cb7de499c785374da8958536
v-jindal <vanshikajindal548@gmail.com>
2026-08-03 09:23:06 +0530

    Auto Backup - 2026-08-03 09:23

---
+++ Tree/Leetcode104.java
@@ -0,0 +1,16 @@
+public class Leetcode104 {
+    public static int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return 1 + Math.max(left, right);
+    }
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        System.out.println(maxDepth(root));
+    }
+}
