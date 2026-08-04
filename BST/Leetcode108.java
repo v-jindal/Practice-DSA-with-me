33ab40aacdee3294b00467d3b3099e85d37d7e40
v-jindal <vanshikajindal548@gmail.com>
2026-08-05 00:56:16 +0530

    Auto Backup - 2026-08-05 00:56

--- BST/Leetcode108.java
+++ BST/Leetcode108.java
@@ -1,6 +1,20 @@
 public class Leetcode108 {
-    
+    public static TreeNode sortedArrayToBST(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        TreeNode tree = helper(left, right, nums);
+        return tree;
+    }
+    public static TreeNode helper(int left, int right, int[] nums){
+        if(left > right) return null;
+        int mid = left + (right - left) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = helper(left, mid - 1, nums);
+        node.right = helper(mid + 1, right, nums);
+        return node;
+    }
     public static void main(String[] args) {
-    
+        int[] nums = {-10, -3, 0, 5, 9};
+        sortedArrayToBST(nums);
     }
 }
