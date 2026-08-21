92ec2a92035f07508d291247886c6cd5872f0ce2
v-jindal <vanshikajindal548@gmail.com>
2026-08-21 09:48:39 +0530

    Auto Backup - 2026-08-21 09:48

--- Graph/Leetcode785.java
+++ Graph/Leetcode785.java
@@ -1,3 +1,32 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 public class Leetcode785 {
-    
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+        int[] color = new int[n];
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < n; i++){
+            if(color[i] == 0){
+                color[i] = 1;
+                q.add(i);
+                if(!bfs(graph, q, color)) return false;
+            }
+        }
+        return true;
+    }
+    public boolean bfs(int[][] graph, Queue<Integer> q, int[] color){
+        while(!q.isEmpty()){
+            int node = q.poll();
+            for(int i : graph[node]){
+                if(color[i] == 0){
+                    q.add(i);
+                    color[i] = color[node] == 1 ? 2 : 1;
+                } else if(color[i] == color[node]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
